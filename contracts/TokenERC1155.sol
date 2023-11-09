34f487a21ba09e7ab3f93574097e2d341cb51b8a
Muskan1615 <vaishmuskan531@gmail.com>
2023-11-09 10:28:46 +0530

    Made change to token erc1155

--- contracts/TokenERC1155.sol
+++ contracts/TokenERC1155.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract TokenERC1155 is ERC1155 {
-    uint256 public constant GOLD = 1;
-    uint256 public constant NFT = 2;
-
     constructor() ERC1155("TokenERC1155") {}
 
     function mint(
